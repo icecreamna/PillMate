0c3832798e9158aff28523f1d0203c10b6e3b5f9
kittabeth <kittabeth2547@gmail.com>
2025-06-28 01:23:14 +0700

    login

--- frontend/lib/screens/auth/login_screen.dart
+++ frontend/lib/screens/auth/login_screen.dart
@@ -10,58 +10,122 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: color.AppColors.backgroundColor1st,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: Image.asset(
-                  "assets/images/drugs.png",
-                  height: 191,
-                  width: 191,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: Image.asset(
+                    "assets/images/drugs.png",
+                    height: 191,
+                    width: 191,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 15),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
+                const SizedBox(height: 15),
+                Container(
+                  height: 514,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                  ),
+                  width: double.infinity,
+                  child: SizedBox.expand(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 12),
+                        const Text(
+                          "Welcome",
+                          style: TextStyle(
+                            color: Color(0xFF0B0870),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          "Please log in to continue",
+                          style: TextStyle(
+                            color: Color(0xFF0B0870),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+                        const TextFieldInput(
+                          labelname: "ID card number",
+                          preIcon: Icon(
+                            Icons.people_outlined,
+                            color: Colors.black,
+                          ),
+                          sufIcon: Icon(
+                            Icons.visibility_off_outlined,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 25),
+                        const TextFieldInput(
+                          labelname: "Password",
+                          preIcon: Icon(
+                            Icons.lock_open_outlined,
+                            color: Colors.black,
+                          ),
+                          sufIcon: Icon(
+                            Icons.visibility_off_outlined,
+                            color: Colors.black,
+                          ),
+                          isEye: true,
+                          hideText: true,
+                        ),
+                        const Spacer(),
+                        FilledButton(
+                          onPressed: () {},
+                          style: FilledButton.styleFrom(
+                            minimumSize: const Size(288, 49),
+                            backgroundColor: color.AppColors.buttonColor,
+                          ),
+                          child: const Text(
+                            "SIGN IN",
+                            style: TextStyle(color: Colors.white, fontSize: 24),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Don't have an account?",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 15,
+                              ),
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                padding: const EdgeInsets.all(5),
+                                minimumSize: const Size(0, 0),
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15,
+                                  color: color.AppColors.buttonColor,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
                 ),
-                width: double.infinity,
-                height: 514,
-                child: const Column(
-                  children: [
-                    SizedBox(height: 12),
-                    Text(
-                      "Welcome",
-                      style: TextStyle(
-                        color: Color(0xFF0B0870),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Please log in to continue",
-                      style: TextStyle(
-                        color: Color(0xFF0B0870),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 13,
-                      ),
-                    ),
-                    TextFieldInput(
-                      labelname: "User",
-                      preIcon: Icon(Icons.lock_open_outlined),
-                      sufIcon: Icon(Icons.visibility_off_outlined),
-                      isEye: true,
-                    ),
-                   
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- frontend/lib/widgets/text_field_input.dart
+++ frontend/lib/widgets/text_field_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/utils/colors.dart' as color;
 
 class TextFieldInput extends StatelessWidget {
   final Icon preIcon;
@@ -12,17 +13,28 @@ class TextFieldInput extends StatelessWidget {
     required this.labelname,
     required this.preIcon,
     required this.sufIcon,
-    required this.isEye,
+    this.isEye = false,
     this.hideText = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        prefixIcon: preIcon,
-        suffixIcon: isEye ? sufIcon : null,
-        labelText: labelname,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: preIcon,
+          suffixIcon: isEye ? sufIcon : null,
+          labelText: labelname,
+          labelStyle: const TextStyle(color: Colors.grey),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: color.AppColors.buttonColor),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: color.AppColors.buttonColor)
+          )
+        ),
+        obscureText: hideText,
       ),
     );
   }
